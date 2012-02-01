72dd8500c0c7a6b37ca31d43b5026c768737ec68
Ilkka Laukkanen <ilkka.laukkanen@futurice.com>
2012-02-01 13:40:14 +0200

    Add command line arg handling to review script
    The script now updates the 'review' remote automatically. Also command line
    args are handled, pass '-h' to get help. Basically, one can now specify
    revisions for review with the '-r' argument.

--- review.sh
+++ review.sh
@@ -5,6 +5,32 @@ die() {
 	exit $2
 }
 
+usage() {
+	case "$#" in
+		2)
+			echo $1 >&2
+			exitstatus=$2
+			;;
+		1)
+			exitstatus=$1
+			;;
+		0)
+			exitstatus=0
+			;;
+	esac
+	echo "Usage: $0 [OPTIONS]"
+	echo
+	echo "Where OPTIONS are one or more of:"
+	echo "	-r (revision | start:end)	Revision or revision range to submit"
+	echo "					for review"
+	echo "	-h				Output help"
+	echo
+	echo "Any other args are passed verbatim to RBTools' post-review."
+	echo "Useful ones include:"
+	echo "	--publish			Immediately publish review request."
+	exit $exitstatus
+}
+
 # check programs and preconditions
 [ -n "$(which git)" ] || die "Cannot find git in PATH" 1
 [ -n "$(which post-review)" ] || die "Cannot find post-review in PATH" 2
@@ -16,19 +42,52 @@ die() {
 git remote|grep -q review || \
 	die "Please add git remote for reviewboard as 'review'" 5
 
-# get changesets
-read lastrev firstrev \
-	<<< $(git log --format=format:%H review/master..master|tr "\\n" " ")
+# read options
+while [ ! -z "$1" ]; do
+	case "$1" in
+		-h)
+			usage
+			;;
+		-r)	# revision or revision range
+			shift
+			[ -n "$1" ] || usage "-r needs an argument" 1
+			if echo "$1"|grep -q ':'; then
+				# two revs were given
+				gitrevarg=$(echo "$1"|awk -F: '{print $1 ".." $2}')
+				revarg="--revision-range=$1"
+			else
+				# only single rev
+				gitrevarg="$1^..$1"
+				revarg="$1"
+			fi
+			;;
+		*)
+			break
+			;;
+	esac
+	shift
+done
 
-if [ -z "$firstrev" ]; then
-	firstrev=$lastrev
-	revarg="$lastrev"
-else
-	revarg="--revision-range=$firstrev:$lastrev"
+# update the review remote
+git fetch review || die "Could not update 'review' remote" 42
+
+# if revargs not given, use review/master and master
+if [ -z "$revarg" ]; then
+	# get changesets
+	read lastrev firstrev \
+		<<< $(git log --format=format:%H review/master..master|tr "\\n" " ")
+
+	if [ -z "$firstrev" ]; then
+		revarg="$lastrev"
+		gitrevarg="$lastrev^..$lastrev"
+	else
+		revarg="--revision-range=$firstrev:$lastrev"
+		gitrevarg="$firstrev..$lastrev"
+	fi
 fi
 
 # prompt for okay
-git --no-pager log --oneline $firstrev^..$lastrev
+git --no-pager log --oneline $gitrevarg
 echo "Push these revisions for review?"
 select yesno in "Yes" "No"; do
 	case $yesno in
@@ -48,5 +107,6 @@ git push review master || die "Git push failed" 6
 post-review --guess-summary --guess-description $@ $revarg || \
 	die "post-review failed" 7
 
+echo
 echo "Done! Don't forget to publish the review request (unless you specified"
 echo "--publish)."
