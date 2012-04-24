98588af606952a98fcb13ae4d0b2cbd91db4c73e
Tuomas Korjonen <tuomas.korjonen@futurice.com>
2012-04-24 15:43:03 +0300

    changed gesturehandler to use weakreference to form

--- Ports/Nokia/S40/src/com/nokia/lwuit/GestureHandler.java
+++ Ports/Nokia/S40/src/com/nokia/lwuit/GestureHandler.java
@@ -11,6 +11,7 @@ import com.nokia.mid.ui.gestures.GestureListener;
 import com.sun.lwuit.Display;
 import com.sun.lwuit.Form;
 import com.sun.lwuit.impl.s40.S40Implementation;
+import java.lang.ref.WeakReference;
 
 /**
  *
@@ -18,16 +19,17 @@ import com.sun.lwuit.impl.s40.S40Implementation;
  */
 public abstract class GestureHandler implements GestureListener{
     
-    private Form form;
+    private WeakReference form;
     
     public GestureHandler(Form f) {
-        form = f;
+        form = new WeakReference(f);
         S40Implementation impl = (S40Implementation)Display.getInstance().getImplementation();
         impl.addGestureHandler(this);
     }
 
     public Form getForm() {
-        return form;
+        Object o = form.get();
+        return (o != null) ? (Form)o : null;
     }
 
     public void gestureAction(Object container, GestureInteractiveZone gestureInteractiveZone, GestureEvent gestureEvent) {
