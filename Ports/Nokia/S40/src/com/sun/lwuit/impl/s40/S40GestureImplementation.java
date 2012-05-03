6a3715faa723fb4b8aba58d1954274ee3b997160
Tuomas Korjonen <tuomas.korjonen@futurice.com>
2012-05-03 10:01:39 +0300

    modified gestures to a more simple version

--- Ports/Nokia/S40/src/com/sun/lwuit/impl/s40/S40GestureImplementation.java
+++ Ports/Nokia/S40/src/com/sun/lwuit/impl/s40/S40GestureImplementation.java
@@ -4,8 +4,7 @@
  */
 package com.sun.lwuit.impl.s40;
 
-import com.nokia.lwuit.FormGestureHandler;
-import com.nokia.lwuit.GlobalGestureHandler;
+import com.nokia.lwuit.GestureHandler;
 import com.nokia.mid.ui.gestures.GestureEvent;
 import com.nokia.mid.ui.gestures.GestureInteractiveZone;
 import com.nokia.mid.ui.gestures.GestureListener;
@@ -18,8 +17,8 @@ import java.util.Vector;
  */
 public class S40GestureImplementation extends S40Implementation{
     
-    private FormGestureHandler currentFormGestureHandler;
-    private GlobalGestureHandler globalGestureHandler;
+    private GestureHandler currentFormGestureHandler;
+    private GestureHandler globalGestureHandler;
         
     private Vector gestureListeners = new Vector();
     private GestureListenerImpl internalListener;
@@ -34,16 +33,16 @@ public class S40GestureImplementation extends S40Implementation{
         GestureRegistrationManager.setListener(canvas, internalListener);
     }
         
-    public void addGestureHandler(FormGestureHandler l) {
+    public void addGestureHandler(GestureHandler l) {
         gestureListeners.addElement(l);
     }
     
     public synchronized void setCurrentGestureListener(com.sun.lwuit.Form f) {
         int l = gestureListeners.size();
-        FormGestureHandler h = null;
-        FormGestureHandler c = null;
+        GestureHandler h = null;
+        GestureHandler c = null;
         for(int i = 0; i < l; i++) {
-            c = (FormGestureHandler) gestureListeners.elementAt(i);
+            c = (GestureHandler) gestureListeners.elementAt(i);
             if(c.getForm() == f) {
                 h = c;
                 break;
@@ -52,11 +51,11 @@ public class S40GestureImplementation extends S40Implementation{
         currentFormGestureHandler = h;
     }
     
-    public synchronized void removeGestureHandler(FormGestureHandler l) {
+    public synchronized void removeGestureHandler(GestureHandler l) {
         gestureListeners.removeElement(l);
     }
 
-    public synchronized void setGlobalGestureHandler(GlobalGestureHandler l) {
+    public synchronized void setGlobalGestureHandler(GestureHandler l) {
         globalGestureHandler = l;
     }
     
@@ -64,7 +63,7 @@ public class S40GestureImplementation extends S40Implementation{
 
         public void gestureAction(Object container, GestureInteractiveZone gestureInteractiveZone, GestureEvent gestureEvent) {
             if(currentFormGestureHandler != null) {
-            currentFormGestureHandler.gestureEvent(gestureEvent);
+            currentFormGestureHandler.gestureAction(gestureEvent);
         }
         if(globalGestureHandler != null) {
             globalGestureHandler.gestureAction(gestureEvent);
