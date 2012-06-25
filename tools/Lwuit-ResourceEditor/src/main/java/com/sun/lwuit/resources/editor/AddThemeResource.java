cb93d14292f5dcff5c21e3cf427c3509e183cd03
Tuomas Korjonen <tuomas.korjonen@futurice.com>
2012-06-25 10:03:03 +0300

    added the ft to resourceeditor gui

--- tools/Lwuit-ResourceEditor/src/main/java/com/sun/lwuit/resources/editor/AddThemeResource.java
+++ tools/Lwuit-ResourceEditor/src/main/java/com/sun/lwuit/resources/editor/AddThemeResource.java
@@ -89,7 +89,7 @@ public class AddThemeResource extends javax.swing.JDialog {
         jLabel2.setText("Template");
         jLabel2.setName("jLabel2"); // NOI18N
 
-        template.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Blank", "nokia_non_touch_theme", "nokia_touch_type_theme", "LWUIT_Touch", "LWUIT_Feature", "tzone_theme", "tipster_theme", "Blackberry", "LWUIT_Classic_Theme", "Wood", "Accessability_Big_Theme", "Accessability_Black_Theme", "Accessability_White_Theme", "Accessability_Yellow_Theme" }));
+        template.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Blank", "nokia_non_touch_theme", "nokia_touch_type_theme", "full_touch_theme", "LWUIT_Touch", "LWUIT_Feature", "tzone_theme", "tipster_theme", "Blackberry", "LWUIT_Classic_Theme", "Wood", "Accessability_Big_Theme", "Accessability_Black_Theme", "Accessability_White_Theme", "Accessability_Yellow_Theme", " " }));
         template.setName("template"); // NOI18N
         template.addActionListener(formListener);
 
