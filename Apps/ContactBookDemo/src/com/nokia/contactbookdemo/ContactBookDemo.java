1b59c1efb061639c385b59dd8a3de8699233529a
Timo Saarinen <timo.saarinen@futurice.com>
2012-07-23 14:38:18 +0300

    Added Clear button (instead of Cmd) to ContactBookDemo

--- Apps/ContactBookDemo/src/com/nokia/contactbookdemo/ContactBookDemo.java
+++ Apps/ContactBookDemo/src/com/nokia/contactbookdemo/ContactBookDemo.java
@@ -11,6 +11,7 @@ package com.nokia.contactbookdemo;
 import com.sun.lwuit.*;
 import com.sun.lwuit.events.ActionEvent;
 import com.sun.lwuit.events.ActionListener;
+import com.sun.lwuit.layouts.BorderLayout;
 import com.sun.lwuit.layouts.BoxLayout;
 import com.sun.lwuit.list.DefaultListModel;
 import java.util.Enumeration;
@@ -20,47 +21,58 @@ import javax.microedition.pim.ContactList;
 import javax.microedition.pim.PIM;
 import javax.microedition.pim.PIMException;
 
-public class ContactBookDemo extends MIDlet {
+public class ContactBookDemo
+    extends MIDlet {
+
     private List contactList = null;
     private PIM pim = null;
-    private Command clearCommand;
-    
+
     public void startApp() {
         Display.init(this);
-        Form form = new Form("Hello world");
-        
+        Form form = new Form("ContactBookDemo");
+
         Command exitCommand = new Command("Exit") {
+
             public void actionPerformed(ActionEvent e) {
                 notifyDestroyed();
             }
         };
         form.addCommand(exitCommand);
         form.setBackCommand(exitCommand);
-        
+
         form.setLayout(new BoxLayout(BoxLayout.Y_AXIS));
         contactList = new List();
         pim = PIM.getInstance();
         form.addComponent(contactList);
-        
+
         final TextArea searchField = TextField.create();
         form.addComponent(searchField);
+
         Button searchButton = new Button("Search");
-        form.addComponent(searchButton);
+        searchButton.setPreferredW(form.getWidth() / 2 - 5);
         searchButton.addActionListener(new ActionListener() {
+
             public void actionPerformed(ActionEvent evt) {
                 populateContactList(searchField.getText());
             }
         });
-        populateContactList(searchField.getText());
-        
-        clearCommand = new Command("Clear") {
+        Button clearButton = new Button("Clear");
+        clearButton.setPreferredW(form.getWidth() / 2 - 5);
+        clearButton.addActionListener(new ActionListener() {
+
             public void actionPerformed(ActionEvent evt) {
                 searchField.setText("");
                 populateContactList("");
             }
-        };
-        form.addCommand(clearCommand);
-        
+        });
+        Container buttonContainer = new Container();
+        buttonContainer.setLayout(new BorderLayout());
+        buttonContainer.addComponent(BorderLayout.WEST, searchButton);
+        buttonContainer.addComponent(BorderLayout.EAST, clearButton);
+        form.addComponent(buttonContainer);
+
+        populateContactList(searchField.getText());
+
         form.show();
     }
 
@@ -70,14 +82,15 @@ public class ContactBookDemo extends MIDlet {
             String[] pimListNames = pim.listPIMLists(PIM.CONTACT_LIST);
             for (int i = 0; i < pimListNames.length; ++i) {
                 ContactList cl = (ContactList) pim.openPIMList(
-                        PIM.CONTACT_LIST, PIM.READ_ONLY, pimListNames[i]);
+                    PIM.CONTACT_LIST, PIM.READ_ONLY, pimListNames[i]);
                 Enumeration items = cl.items(searchTerm);
                 while (items.hasMoreElements()) {
                     Contact c = (Contact) items.nextElement();
                     contactList.addItem(c.getString(Contact.FORMATTED_NAME, 0));
                 }
             }
-        } catch (PIMException ex) {
+        }
+        catch (PIMException ex) {
             ex.printStackTrace();
         }
         if (contactList.getModel().getSize() == 0) {
